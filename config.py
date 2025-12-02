84874bd3df8509ac58527e65f95080e3528be4cb
Afreenshaik07 <afreenshaik0711@gmail.com>
2025-12-02 21:15:00 +0530

    Fixed Gmail App Password formatting

--- config.py
+++ config.py
@@ -1,4 +1,7 @@
+# config.py
+
 EMAIL_ADDRESS = "d88368817@gmail.com"
-EMAIL_PASSWORD = "pehg gord dkbg lred"
+EMAIL_PASSWORD = "pehggorddkbglred"   # app password WITHOUT spaces
+
 ADMIN_ID = "admin"
 ADMIN_PASSWORD = "admin"
